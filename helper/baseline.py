7ea0c082de1d3005d423967e88f5ead4f3ac7a53
Marc Bruni <marc.bruni@students.fhnw.ch>
2024-06-12 18:01:59 +0900

    add script for baseline prompt

---
+++ helper/baseline.py
@@ -0,0 +1,24 @@
+import json
+import os
+
+from dotenv import load_dotenv
+
+from project_types.custom_types import Approach
+from utils import relative_path_from_root
+
+load_dotenv()
+data_file_path = relative_path_from_root(os.getenv('DATA_FILE_PATH'))
+
+with open(data_file_path, 'r') as file:
+    data = json.load(file)
+
+approach = Approach(**data)
+samples = approach.attempt.data
+
+for sample in samples:
+    sample.modified_prompt = sample.original_prompt
+
+file_name, file_extension = os.path.splitext(data_file_path)
+scanned_data_file_path = f"{file_name}_baseline{file_extension}"
+with open(scanned_data_file_path, 'w') as file:
+    json.dump(approach.dict(exclude_unset=True), file, indent=4)
