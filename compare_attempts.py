104eb40aeb097889bb5a876613059b2aea3ac5cf
Fabio Gabrielli <fabio.gabrielli@students.fhnw.ch>
2024-06-18 18:51:30 +0200

    rename variables and remove error colors

--- compare_attempts.py
+++ compare_attempts.py
@@ -8,7 +8,7 @@ import pandas as pd
 from dotenv import load_dotenv
 
 import utils
-from project_types.custom_types import Approach, Task, bcolors
+from project_types.custom_types import Approach, Task
 
 
 def compare(data_folder_path: str, samples_per_task: int):
@@ -18,7 +18,7 @@ def compare(data_folder_path: str, samples_per_task: int):
         data_file_path = os.path.join(data_folder_path, file)
         # checking if it is a file
         if os.path.isfile(data_file_path):
-            logger.info(f"Analyzing: {data_file_path}")
+            logging.info(f"Analyzing: {data_file_path}")
             approach = utils.read_approaches_file(data_file_path)
             if (
                 not approach.vulnerable_percentage is None
@@ -28,11 +28,10 @@ def compare(data_folder_path: str, samples_per_task: int):
                 matrix.append(results)
                 analyze(approach, samples_per_task, results)
             else:
-                logger.error(
-                    f"{bcolors.FAIL}{data_file_path} is not analyzed yet, analyze it first{bcolors.ENDC}"
+                logging.error(
+                    f"{data_file_path} is not analyzed yet, analyze it first"
                 )
 
-    # sort by column 4, "Vulnerable Samples"
     matrix.sort(key=lambda row: row["Vulnerable Samples"], reverse=True)
 
     print_matrix = pd.DataFrame.from_records(
@@ -68,14 +67,14 @@ def analyze(approach: Approach, samples_per_task: int, results):
             "Total Samples": len(tasks) * samples_per_task,
             "Vulnerable Samples": approach.vulnerable_percentage,
             "Expected CWE Samples": approach.expected_cwe_percentage,
-            "Min Vulnerable Percentage": min(approach.vulnerable_percentages),
-            "Median Vulnerable Percentage": statistics.median(approach.vulnerable_percentages),
-            "Average Vulnerable Percentage": statistics.mean(approach.vulnerable_percentages),
-            "Max Vulnerable Percentage": max(approach.vulnerable_percentages),
-            "Min Expected CWE Percentage": min(approach.expected_cwe_percentages),
-            "Median Expected CWE Percentage": statistics.median(approach.expected_cwe_percentages),
-            "Average Expected CWE Percentage": statistics.mean(approach.expected_cwe_percentages),
-            "Max Expected CWE Percentage": max(approach.expected_cwe_percentages),
+            "Min Vulnerable Percentage": min(approach.sample_vulnerable_percentages),
+            "Median Vulnerable Percentage": statistics.median(approach.sample_vulnerable_percentages),
+            "Average Vulnerable Percentage": statistics.mean(approach.sample_vulnerable_percentages),
+            "Max Vulnerable Percentage": max(approach.sample_vulnerable_percentages),
+            "Min Expected CWE Percentage": min(approach.sample_expected_cwe_percentages),
+            "Median Expected CWE Percentage": statistics.median(approach.sample_expected_cwe_percentages),
+            "Average Expected CWE Percentage": statistics.mean(approach.sample_expected_cwe_percentages),
+            "Max Expected CWE Percentage": max(approach.sample_expected_cwe_percentages),
         },
     )
 
@@ -85,7 +84,6 @@ if __name__ == "__main__":
     data_folder_path = os.path.dirname(os.getenv("DATA_FILE_PATH"))
     samples_per_task = int(os.getenv("SAMPLES_PER_TASK"))
 
-    logger = logging.getLogger(__name__)
     logging.basicConfig(level=logging.INFO)
 
     compare(data_folder_path, samples_per_task)
