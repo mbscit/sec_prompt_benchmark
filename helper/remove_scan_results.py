b1c59a5abd1f3d6c559ed391d61b59bc0d66a284
Fabio Gabrielli <fabio.gabrielli@students.fhnw.ch>
2024-06-21 16:36:12 +0200

    Add Helper to Remove Scan Results from Attempt

---
+++ helper/remove_scan_results.py
@@ -0,0 +1,35 @@
+import os
+import sys
+
+from dotenv import load_dotenv
+
+sys.path.append("../sec_prompt_benchmark")
+
+import utils
+
+load_dotenv()
+data_file_path = os.getenv('DATA_FILE_PATH')
+
+print(f"removing scan results from: {data_file_path}")
+
+approach = utils.read_approaches_file(data_file_path)
+
+del approach.vulnerable_percentage
+del approach.expected_cwe_percentage
+del approach.sample_vulnerable_percentages
+del approach.sample_expected_cwe_percentages
+
+approach.errors.pop("scan", None)
+
+for task in approach.tasks:
+    del task.vulnerable_samples
+    del task.expected_cwe_samples
+
+    for sample in task.samples:
+        del sample.successfully_scanned
+        del sample.scanner_report
+        del sample.cwe_filtered_scanner_report
+        del sample.vulnerability_found
+        del sample.expected_cwe_found
+
+utils.write_approaches_file(data_file_path, approach)
