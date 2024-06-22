7d265830dde3633eedbca1d9ce85465860e9e6f5
Fabio Gabrielli <fabio.gabrielli@students.fhnw.ch>
2024-06-22 15:03:37 +0200

    update basic create script and rename datasets

--- attempts/baseline.py
+++ attempts/baseline.py
@@ -9,6 +9,13 @@ sys.path.append("../sec_prompt_benchmark")
 import utils
 from project_types.custom_types import Approach, Prompt, Task
 
+# START Attempt Specific Changes
+attempt_name = "baseline"
+attempt_description = "Baseline - no prompt modification"
+prompt_prefix = ""
+prompt_suffix = ""
+# END Attempt Specific Changes
+
 
 def create_approach(dataset: List[Prompt]) -> Approach:
 
@@ -18,15 +25,15 @@ def create_approach(dataset: List[Prompt]) -> Approach:
         task = Task(
             id=prompt.id,
             original_prompt=prompt.prompt,
-            modified_prompt=prompt.prompt,  # TODO modify prompt
+            modified_prompt=prompt_prefix + prompt.prompt + prompt_suffix,
             suspected_vulnerability=prompt.suspected_vulnerability,
             language=prompt.language,
         )
         tasks.append(task)
 
     approach = Approach(
-        id="llmseceval_baseline",
-        description="Baseline - no prompt modification",
+        id=attempt_name,
+        description=attempt_description,
         tasks=tasks,
     )
 
@@ -35,7 +42,9 @@ def create_approach(dataset: List[Prompt]) -> Approach:
 
 if __name__ == "__main__":
     load_dotenv()
-    data_file_path = os.getenv("DATA_FILE_PATH")
+    data_folder_path = os.path.dirname(os.getenv("DATA_FILE_PATH"))
+    data_file_path = os.path.join(data_folder_path, attempt_name + ".json")
+
     dataset_file_path = os.getenv("DATASET_FILE_PATH")
 
     dataset = utils.read_dataset_file(dataset_file_path)
