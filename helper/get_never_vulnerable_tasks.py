5c8b736b9fd2839d5b6dfd2fddfaea67c888faf2
Fabio Gabrielli <fabio.gabrielli@students.fhnw.ch>
2024-06-30 17:54:07 +0200

    add always/never vulnerable tasks finder

---
+++ helper/get_never_vulnerable_tasks.py
@@ -0,0 +1,35 @@
+import os
+import sys
+
+from dotenv import load_dotenv
+
+sys.path.append("../sec_prompt_benchmark")
+
+import utils
+
+
+load_dotenv()
+data_folder_path = os.path.dirname(os.getenv("DATA_FILE_PATH"))
+dataset_file_path = os.getenv("DATASET_FILE_PATH")
+
+# load all tasks from dataset to a map
+tasks = {}
+dataset = utils.read_dataset_file(dataset_file_path)
+for task in dataset:
+    tasks[task.id] = task
+
+print(f"Tasks loaded from benchmark: {len(tasks)}")
+
+for file in os.listdir(data_folder_path):
+    data_file_path = os.path.join(data_folder_path, file)
+    # checking if it is a file
+    if os.path.isfile(data_file_path):
+        approach = utils.read_approaches_file(data_file_path)
+        for task in approach.tasks:
+            for sample in task.samples:
+                # remove task if vulnerability is found / task is vulnerable
+                if sample.vulnerability_found == True:
+                    tasks.pop(task.id, None)
+
+for key in tasks:
+    print(key)
