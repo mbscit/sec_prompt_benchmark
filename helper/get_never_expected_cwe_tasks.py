b8c209c62b21ae0a978a012a138c1c28ab5df9a4
Marc Bruni <marc.bruni@students.fhnw.ch>
2024-07-04 18:22:37 +0900

    remove all tasks that have never been classified with expected cwe by semgrep

---
+++ helper/get_never_expected_cwe_tasks.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+from dotenv import load_dotenv
+
+sys.path.append("../sec_prompt_benchmark")
+
+import utils
+
+
+load_dotenv()
+data_folder_path = os.path.dirname(utils.relative_path_from_root(os.getenv("DATA_FILE_PATH")))
+dataset_file_path = utils.relative_path_from_root(os.getenv("DATASET_FILE_PATH"))
+
+# load all tasks from dataset to a map
+tasks = {}
+dataset = utils.read_dataset_file(dataset_file_path)
+for task in dataset:
+    tasks[task.id] = task
+
+print(f"Tasks loaded from benchmark: {len(tasks)}")
+
+for file in os.listdir(data_folder_path):
+    data_file_path = os.path.join(data_folder_path, file)
+    # checking if it is a file
+    if os.path.isfile(data_file_path):
+        approach = utils.read_approaches_file(data_file_path)
+        for task in approach.tasks:
+            for sample in task.samples:
+                # remove task if vulnerability is found / task is vulnerable
+                if sample.expected_cwe_found == True:
+                    tasks.pop(task.id, None)
+
+print(f"Tasks that have NEVER been classified with the suspected CWE (count: {len(tasks)}): ")
+for key in tasks:
+    print(f"\"{key}\",")
