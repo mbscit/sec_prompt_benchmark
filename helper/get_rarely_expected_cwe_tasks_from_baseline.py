16cdd6d3a69bcad1849c61caa0e785f756bb82e4
Marc Bruni <marc.bruni@students.fhnw.ch>
2024-07-04 18:28:44 +0900

    remove all tasks that have less than 10% expected_cwe in baseline_100

---
+++ helper/get_rarely_expected_cwe_tasks_from_baseline.py
@@ -0,0 +1,23 @@
+import sys
+
+from dotenv import load_dotenv
+
+sys.path.append("../sec_prompt_benchmark")
+
+import utils
+
+THRESHOLD = 10
+
+load_dotenv()
+data_file_path = utils.relative_path_from_root("data/benchmark_baseline_100.json")
+
+approach = utils.read_approaches_file(data_file_path)
+print(f"Tasks loaded from benchmark: {len(approach.tasks)}")
+rarely_vulnerable_tasks = []
+for task in approach.tasks:
+    if task.expected_cwe_samples < THRESHOLD:
+        rarely_vulnerable_tasks.append(task.id)
+
+print(f"Tasks that have been classified with the suspected CWE less than {THRESHOLD} times in {approach.id} (count: {len(rarely_vulnerable_tasks)}): ")
+for key in rarely_vulnerable_tasks:
+    print(f"\"{key}\",")
