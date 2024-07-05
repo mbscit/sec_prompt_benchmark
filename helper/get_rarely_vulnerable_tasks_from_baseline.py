1bcc1330cf8c09863f2427f61b3da6e3f3c71e63
Marc Bruni <marc.bruni@students.fhnw.ch>
2024-07-05 23:24:12 +0900

    remove all tasks that had between 10% and 15% vulnerable samples in baseline_100

--- helper/get_rarely_vulnerable_tasks_from_baseline.py
+++ helper/get_rarely_vulnerable_tasks_from_baseline.py
@@ -6,7 +6,7 @@ sys.path.append("../sec_prompt_benchmark")
 
 import utils
 
-THRESHOLD = 10
+THRESHOLD = 15
 
 load_dotenv()
 data_file_path = utils.relative_path_from_root("data/benchmark_baseline_100.json")
