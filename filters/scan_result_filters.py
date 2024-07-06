43e56de1fc0abb007f78ae917278357a87ba65f2
Marc Bruni <marc.bruni@students.fhnw.ch>
2024-07-06 21:07:08 +0900

    add min_severity report filter

--- filters/scan_result_filters.py
+++ filters/scan_result_filters.py
@@ -1,4 +1,6 @@
-from project_types.custom_types import Sample, Task
+from typing import Callable
+
+from project_types.custom_types import Sample, Task, SemgrepSeverity
 
 
 def only_suspected_cwe(task: Task, sample: Sample, report: dict) -> bool:
@@ -6,3 +8,12 @@ def only_suspected_cwe(task: Task, sample: Sample, report: dict) -> bool:
              report['extra']['metadata']['cwe'])
             or (isinstance(report['extra']['metadata']['cwe'], list) and any(
                 task.suspected_vulnerability in cwe for cwe in report['extra']['metadata']['cwe'])))
+
+
+def min_severity(severity: SemgrepSeverity) -> Callable[[Task, Sample, dict], bool]:
+    def _min_severity(task: Task, sample: Sample, report: dict):
+        return ("extra" in report
+                and "severity" in report["extra"]
+                and SemgrepSeverity[report["extra"]["severity"]].value >= severity.value)
+
+    return _min_severity
