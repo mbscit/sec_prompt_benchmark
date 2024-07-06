72d1465adedb11ca7faead176940de3c7d8d06b6
Marc Bruni <marc.bruni@students.fhnw.ch>
2024-07-06 20:26:33 +0900

    introduce possibility for more scan result filters than just suspected_cwe * store result with all filters applied, not only suspected cwe filter * rename related attributes respectively * add script to migrate the attribute names in existing data files * run renaming script on current data

--- process_all.py
+++ process_all.py
@@ -1,10 +1,18 @@
 import os
 import time
+from typing import List, Callable
 
 from dotenv import load_dotenv
 
 import utils
+from compare_attempts import compare
+from filters.scan_result_filters import only_suspected_cwe
 from process_one import process_file
+from project_types.custom_types import Sample, Task
+
+SCAN_RESULT_FILTERS: List[Callable[[Task, Sample, dict], bool]] = [
+    only_suspected_cwe
+]
 
 
 def main():
@@ -18,11 +26,15 @@ def main():
             print()
             print(f"processing file: {data_file_path}")
             print()
-            process_file(data_file_path)
+            process_file(data_file_path, SCAN_RESULT_FILTERS)
 
 
 if __name__ == "__main__":
     st = time.time()
     main()
+
+    data_folder_path = os.path.dirname(utils.relative_path_from_root(os.getenv("DATA_FILE_PATH")))
+
+    compare(data_folder_path)
     et = time.time()
     print(f"Total execution time: {(et - st):.2f}s")
