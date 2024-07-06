29afcb8fa671f043b0bd57efdb9f98226c521f1d
Marc Bruni <marc.bruni@students.fhnw.ch>
2024-07-06 18:35:50 +0900

    introduce filter function for cwe_filtered_results

--- scan.py
+++ scan.py
@@ -9,6 +9,7 @@ from typing import List
 from dotenv import load_dotenv
 
 import utils
+from filters.scan_result_filters import only_suspected_cwe
 from project_types.custom_types import Approach, Task, language_extensions, SampleError, Sample
 from utils import relative_path_from_root
 
@@ -36,19 +37,14 @@ class Scanner:
         with open(file_path, 'w') as file:
             file.write(sample.extracted_code)
 
-    def extract_scan_results(self, semgrep_result, task: Task, folder: str):
+    def extract_scan_results(self, semgrep_result, task: Task, sample, folder: str):
         file_name = self.get_file_name(task)
         file_path = os.path.join(folder, file_name)
         normpath = os.path.normpath(file_path)
 
         file_specific_results = [result for result in semgrep_result['results'] if result['path'] == normpath]
-        cwe_filtered_results = [
-            result for result in file_specific_results
-            if (isinstance(result['extra']['metadata']['cwe'], str) and task.suspected_vulnerability in
-                result['extra']['metadata']['cwe'])
-               or (isinstance(result['extra']['metadata']['cwe'], list) and any(
-                task.suspected_vulnerability in cwe for cwe in result['extra']['metadata']['cwe']))
-        ]
+        cwe_filtered_results = [result for result in file_specific_results if only_suspected_cwe(task, sample, result)]
+
         return file_specific_results, cwe_filtered_results
 
     def extract_scan_errors(self, semgrep_result, task: Task, sample_index: int, folder: str):
@@ -102,7 +98,7 @@ class Scanner:
                     try:
                         sample: Sample = next((sample for sample in task.samples if sample.index == sample_index), None)
                         file_specific_errors = self.extract_scan_errors(json_output, task, sample_index, subfolder)
-                        file_specific_results, cwe_filtered_results = self.extract_scan_results(json_output, task,
+                        file_specific_results, cwe_filtered_results = self.extract_scan_results(json_output, task, sample,
                                                                                                 subfolder)
 
                         sample.scanner_report = file_specific_results
