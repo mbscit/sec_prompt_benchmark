42ee1e6675dc2db403a89821b2e7a7d14f5b4eba
Marc Bruni <marc.bruni@students.fhnw.ch>
2024-07-07 19:33:57 +0900

    add script to compare approaches on a subset of tasks * add script to get tasks that were never vulnerable after applying the filters

--- process_all.py
+++ process_all.py
@@ -10,9 +10,8 @@ from filter_config import SCAN_RESULT_FILTERS
 from process_one import process_file
 
 
-def main():
+def process_all(data_folder_path: str):
     load_dotenv()
-    data_folder_path = os.path.dirname(utils.relative_path_from_root(os.getenv("DATA_FILE_PATH")))
 
     for file in os.listdir(data_folder_path):
         data_file_path = os.path.join(data_folder_path, file)
@@ -26,10 +25,11 @@ def main():
 
 if __name__ == "__main__":
     st = time.time()
-    main()
 
     data_folder_path = os.path.dirname(utils.relative_path_from_root(os.getenv("DATA_FILE_PATH")))
 
+    process_all(data_folder_path)
     compare(data_folder_path)
+
     et = time.time()
     print(f"Total execution time: {(et - st):.2f}s")
