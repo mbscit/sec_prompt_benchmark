1288096cc3de07371a9ef6c6fdaf8a37b70e3c25
Marc Bruni <marc.bruni@students.fhnw.ch>
2024-07-07 17:31:32 +0900

    make helper functions to identify rarely/never/always vulnerable tasks return a list of the task ids

--- helper/get_always_vulnerable_tasks.py
+++ helper/get_always_vulnerable_tasks.py
@@ -1,5 +1,7 @@
+import logging
 import os
 import sys
+from typing import List
 
 from dotenv import load_dotenv
 
@@ -8,29 +10,36 @@ sys.path.append("../sec_prompt_benchmark")
 import utils
 
 
-load_dotenv()
-data_folder_path = os.path.dirname(utils.relative_path_from_root(os.getenv("DATA_FILE_PATH")))
-dataset_file_path = utils.relative_path_from_root(os.getenv("DATASET_FILE_PATH"))
+def get_always_vulnerable_tasks(reference_file_path: str) -> List[str]:
+    data_folder_path = os.path.dirname(utils.relative_path_from_root(os.getenv("DATA_FILE_PATH")))
 
-# load all tasks from dataset to a map
-tasks = {}
-dataset = utils.read_dataset_file(dataset_file_path)
-for task in dataset:
-    tasks[task.id] = task
+    approach = utils.read_approaches_file(reference_file_path)
+    logging.info(f"Tasks loaded from reference approach {approach.id}: {len(approach.tasks)}")
+    task_ids = [task.id for task in approach.tasks]
 
-print(f"Tasks loaded from benchmark: {len(tasks)}")
+    for file in os.listdir(data_folder_path):
+        data_file_path = os.path.join(data_folder_path, file)
+        # checking if it is a file
+        if os.path.isfile(data_file_path):
+            approach = utils.read_approaches_file(data_file_path)
+            for task in approach.tasks:
+                for sample in task.samples:
+                    # remove task if no vulnerability is found / task is not vulnerable
+                    if not sample.vulnerability_found:
+                        # remove from task_ids
+                        if task.id in task_ids:
+                            task_ids.remove(task.id)
 
-for file in os.listdir(data_folder_path):
-    data_file_path = os.path.join(data_folder_path, file)
-    # checking if it is a file
-    if os.path.isfile(data_file_path):
-        approach = utils.read_approaches_file(data_file_path)
-        for task in approach.tasks:
-            for sample in task.samples:
-                # remove task if no vulnerability is found / task is not vulnerable
-                if sample.vulnerability_found == False:
-                    tasks.pop(task.id, None)
+    logging.info(f"Tasks that have ALWAYS been classified as vulnerable (count: {len(task_ids)}): ")
+    for task_id in task_ids:
+        logging.info(f"\"{task_id}\",")
 
-print(f"Tasks that have ALWAYS been classified as vulnerable (count: {len(tasks)}): ")
-for key in tasks:
-    print(f"\"{key}\",")
+    return task_ids
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
+    load_dotenv()
+    dataset_file_path = utils.relative_path_from_root(os.getenv("DATA_FILE_PATH"))
+    get_always_vulnerable_tasks(dataset_file_path)
---
+++ helper/get_rarely_vulnerable_tasks_in_reference_run.py
@@ -0,0 +1,35 @@
+import logging
+import sys
+from typing import List
+
+from dotenv import load_dotenv
+
+sys.path.append("../sec_prompt_benchmark")
+
+import utils
+
+load_dotenv()
+
+
+# returns all tasks that have been vulnerable less than threshold_percentage percent in the reference file
+def get_rarely_vulnerable_tasks_from(reference_data_file_path: str, threshold_percentage: int) -> List[str]:
+    approach = utils.read_approaches_file(reference_data_file_path)
+    logging.info(f"Tasks loaded from {approach.id}: {len(approach.tasks)}")
+
+    rarely_vulnerable_tasks: List[str] = []
+    for task in approach.tasks:
+        if task.vulnerable_samples < len(task.samples) * threshold_percentage / 100:
+            rarely_vulnerable_tasks.append(task.id)
+
+    logging.info(
+        f"Tasks that have been vulnerable less than {threshold_percentage} percent in {approach.id} (count: {len(rarely_vulnerable_tasks)}): ")
+    for key in rarely_vulnerable_tasks:
+        logging.info(f"\"{key}\",")
+    return rarely_vulnerable_tasks
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
+    data_file_path = utils.relative_path_from_root("data/benchmark_baseline_100.json")
+    get_rarely_vulnerable_tasks_from(data_file_path, 10)
