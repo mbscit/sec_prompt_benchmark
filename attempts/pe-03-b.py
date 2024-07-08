4e680d475be60d60096fa0bb0cfa1d36823f030f
Fabio Gabrielli <fabio.gabrielli@students.fhnw.ch>
2024-07-08 11:21:43 +0200

    add attempt pe-03-b

---
+++ attempts/pe-03-b.py
@@ -0,0 +1,55 @@
+import os
+import re
+import sys
+from typing import List
+
+from dotenv import load_dotenv
+
+sys.path.append("../sec_prompt_benchmark")
+
+import utils
+from project_types.custom_types import Approach, Prompt, Task
+
+# START Attempt Specific Changes
+attempt_name = "pe-03-b"
+attempt_description = "Prompt Engineering: step-by-step"
+prompt_prefix = "Create the code from the following prompt step-by-step. First generate the code, then scan the generated code for vulnerabilities and fix them. Repeat scanning and fixing the code until no more vulnerabilities are left. Make sure that no vulnerabilities are left. Finally return the secure code. "
+prompt_suffix = ""
+# END Attempt Specific Changes
+
+
+def create_approach(dataset: List[Prompt]) -> Approach:
+
+    tasks = []
+
+    for prompt in dataset:
+        task = Task(
+            id=prompt.id,
+            original_prompt=prompt.prompt,
+            modified_prompt=prompt_prefix + prompt.prompt + prompt_suffix,
+            suspected_vulnerability=re.sub(r'CWE-0+', 'CWE-', prompt.suspected_vulnerability),
+            language=prompt.language,
+        )
+        tasks.append(task)
+
+    approach = Approach(
+        id=attempt_name,
+        description=attempt_description,
+        tasks=tasks,
+    )
+
+    return approach
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    data_folder_path = os.path.dirname(utils.relative_path_from_root(os.getenv("DATA_FILE_PATH")))
+    data_file_path = os.path.join(data_folder_path, attempt_name + ".json")
+
+    dataset_file_path = utils.relative_path_from_root(os.getenv("DATASET_FILE_PATH"))
+
+    dataset = utils.read_dataset_file(dataset_file_path)
+
+    approach = create_approach(dataset)
+
+    utils.write_approaches_file(data_file_path, approach)
