c8dc03f1d345539bafb3c83abaeb09ac696b8923
Marc Bruni <marc.bruni@students.fhnw.ch>
2024-07-10 21:50:15 +0900

    simplify scan result filters by introducing get_detected_cwes function

--- filters/scan_result_filters.py
+++ filters/scan_result_filters.py
@@ -1,4 +1,5 @@
 import os
+import re
 import sys
 from typing import Callable, List
 
@@ -10,11 +11,20 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from cwe_resources import cwe_infos
 
 
+def get_detected_cwes(report: dict) -> List[str]:
+    cwe_messages: List[str] = []
+    if isinstance(report['extra']['metadata']['cwe'], str):
+        cwe_messages: List[str] = [report['extra']['metadata']['cwe']]
+
+    if isinstance(report['extra']['metadata']['cwe'], list):
+        cwe_messages: List[str] = report['extra']['metadata']['cwe']
+
+    cwe_ids = [re.search(r'CWE-\d+', cwe_message).group() for cwe_message in cwe_messages]
+    return cwe_ids
+
+
 def only_suspected_cwe(task: Task, sample: Sample, report: dict) -> bool:
-    return ((isinstance(report['extra']['metadata']['cwe'], str) and task.suspected_vulnerability in
-             report['extra']['metadata']['cwe'])
-            or (isinstance(report['extra']['metadata']['cwe'], list) and any(
-                task.suspected_vulnerability in cwe for cwe in report['extra']['metadata']['cwe'])))
+    return task.suspected_vulnerability in get_detected_cwes(report)
 
 
 def cwe_relatives_of_suspected(allow_ancestors=True, allow_peers=True, allow_descendants=True) -> Callable[
@@ -47,14 +57,10 @@ def cwe_in_recommended_mapping(task: Task, sample: Sample, report: dict) -> bool
 
 
 def only_allow_cwes(report: dict, allowed_cwes):
-    if isinstance(report['extra']['metadata']['cwe'], str):
-        found_cwe: str = report['extra']['metadata']['cwe']
-        return any(allowed_cwe in found_cwe for allowed_cwe in allowed_cwes)
-    if isinstance(report['extra']['metadata']['cwe'], list):
-        found_cwes: List[str] = report['extra']['metadata']['cwe']
-        found_cwes = [found_cwe for found_cwe in found_cwes if
-                      any(allowed_cwe in found_cwe for allowed_cwe in allowed_cwes)]
-        return len(found_cwes) > 0
+    found_cwes: get_detected_cwes(report)
+    found_cwes = [found_cwe for found_cwe in found_cwes if
+                  any(allowed_cwe in found_cwe for allowed_cwe in allowed_cwes)]
+    return len(found_cwes) > 0
 
 
 def min_severity(severity: SemgrepSeverity) -> Callable[[Task, Sample, dict], bool]:
@@ -85,13 +91,9 @@ def affected_line_in_generated_response(task: Task, sample: Sample, report: dict
 
 def ignore_cwes(cwes_to_ignore: List[str]) -> Callable[[Task, Sample, dict], bool]:
     def _ignore_cwes(task: Task, sample: Sample, report: dict):
-        if isinstance(report['extra']['metadata']['cwe'], str):
-            found_cwe: str = report['extra']['metadata']['cwe']
-            return not any(ignored_cwe in found_cwe for ignored_cwe in cwes_to_ignore)
-        if isinstance(report['extra']['metadata']['cwe'], list):
-            found_cwes: List[str] = report['extra']['metadata']['cwe']
-            for ignored_cwe in cwes_to_ignore:
-                found_cwes = [cwe for cwe in found_cwes if ignored_cwe not in cwe]
-            return len(found_cwes) > 0
+        found_cwes: List[str] = get_detected_cwes(report)
+        for ignored_cwe in cwes_to_ignore:
+            found_cwes = [cwe for cwe in found_cwes if ignored_cwe not in cwe]
+        return len(found_cwes) > 0
 
     return _ignore_cwes
