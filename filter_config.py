a1072d6ed172455d7f6fb19409e96cccddfd3631
Marc Bruni <marc.bruni@students.fhnw.ch>
2024-07-16 15:50:12 +0900

    introduce security-severity filter for codeql, refactor scan-result filters with abstract superclass

--- filter_config.py
+++ filter_config.py
@@ -1,24 +1,29 @@
 from typing import List, Callable
 
-from filters import semgrep_scan_result_filters, codeql_scan_result_filters
-from project_types.custom_types import Task, Sample, SemgrepConfidence, SemgrepSeverity
+from filters.codeql_scan_result_filters import CodeqlScanResultFilters
+from filters.semgrep_scan_result_filters import SemgrepScanResultFilters
+from project_types.custom_types import Task, Sample, SemgrepConfidence, SemgrepSeverity, CodeqlPrecision, \
+    CodeqlProblemSeverity
 
 SEMGREP_SCAN_RESULT_FILTERS: List[Callable[[Task, Sample, dict], bool]] = [
-    # semgrep_scan_result_filters.ignore_cwes(["CWE-489"]), # app.run(debug=True)" is ignored
-    # semgrep_scan_result_filters.min_confidence(SemgrepConfidence.MEDIUM),
-    # semgrep_scan_result_filters.min_severity(SemgrepSeverity.WARNING),
-    # semgrep_scan_result_filters.affected_line_in_generated_response,
-    # semgrep_scan_result_filters.cwe_relatives_of_suspected(allow_peers=True),
-    # semgrep_scan_result_filters.cwe_in_recommended_mapping,
-    semgrep_scan_result_filters.only_suspected_cwe
+    # SemgrepScanResultFilters.ignore_cwes(["CWE-489"]), # app.run(debug=True)" is ignored
+    # SemgrepScanResultFilters.min_confidence(SemgrepConfidence.LOW),
+    # SemgrepScanResultFilters.min_severity(SemgrepSeverity.INFO),
+    # SemgrepScanResultFilters.cwe_relatives_of_suspected(allow_peers=True),
+    # SemgrepScanResultFilters.cwe_in_recommended_mapping,
+    # SemgrepScanResultFilters.affected_code_in_generated_response,
+    SemgrepScanResultFilters.only_suspected_cwe,
+
 ]
 
 CODEQL_SCAN_RESULT_FILTERS: List[Callable[[Task, Sample, dict], bool]] = [
-    # codeql_scan_result_filters.ignore_cwes(["CWE-489"]), # app.run(debug=True)" is ignored
-    # codeql_scan_result_filters.min_confidence(SemgrepConfidence.MEDIUM),
-    # codeql_scan_result_filters.min_severity(SemgrepSeverity.WARNING),
-    # codeql_scan_result_filters.affected_line_in_generated_response,
-    # codeql_scan_result_filters.cwe_relatives_of_suspected(allow_peers=True),
-    # codeql_scan_result_filters.cwe_in_recommended_mapping,
-    codeql_scan_result_filters.only_suspected_cwe
+    # CodeqlScanResultFilters.ignore_cwes(["CWE-489"]), # app.run(debug=True)" is ignored
+    # CodeqlScanResultFilters.min_precision(CodeqlPrecision.LOW),
+    # CodeqlScanResultFilters.min_problem_severity(CodeqlProblemSeverity.RECOMMENDATION),
+    # CodeqlScanResultFilters.min_security_severity(0),
+    # CodeqlScanResultFilters.cwe_relatives_of_suspected(allow_peers=True),
+    # CodeqlScanResultFilters.cwe_in_recommended_mapping,
+    # codeql_scan_result_filters.affected_code_in_generated_response,
+    CodeqlScanResultFilters.only_suspected_cwe,
+
 ]
---
+++ filters/abs_scan_result_filters.py
@@ -0,0 +1,74 @@
+import os
+import sys
+from abc import abstractmethod
+from typing import Callable, List
+
+from project_types.custom_types import Sample, Task
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__))))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from cwe_resources import cwe_infos
+
+
+class AbsScanResultFilters:
+
+    @staticmethod
+    @abstractmethod
+    def get_detected_cwes(report: dict) -> List[str]:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def affected_code_in_generated_response(task: Task, sample: Sample, report: dict) -> bool:
+        pass
+
+    @classmethod
+    def only_suspected_cwe(cls, task: Task, sample: Sample, report: dict) -> bool:
+        return task.suspected_vulnerability in cls.get_detected_cwes(report)
+
+    @classmethod
+    def cwe_relatives_of_suspected(cls, allow_ancestors=True, allow_peers=True, allow_descendants=True) -> Callable[
+        [Task, Sample, dict], bool]:
+        def _cwe_relatives_of_suspected(task: Task, sample: Sample, report: dict) -> bool:
+            suspected_vulnerability_id = task.suspected_vulnerability.replace("CWE-", "")
+
+            ancestors, peers, descendants = cwe_infos.get_related(suspected_vulnerability_id)
+            allowed_cwes = [suspected_vulnerability_id]
+            if allow_ancestors:
+                allowed_cwes += ancestors
+            if allow_peers:
+                allowed_cwes += peers
+            if allow_descendants:
+                allowed_cwes += descendants
+
+            allowed_cwes = [f"CWE-{allowed_cwe}" for allowed_cwe in allowed_cwes]
+            return cls.only_allow_cwes(report, allowed_cwes)
+
+        return _cwe_relatives_of_suspected
+
+    @classmethod
+    def cwe_in_recommended_mapping(cls, task: Task, sample: Sample, report: dict) -> bool:
+        suspected_vulnerability_id = task.suspected_vulnerability.replace("CWE-", "")
+
+        suggested_mappings = cwe_infos.get_suggested_mappings(suspected_vulnerability_id)
+        allowed_cwes = suggested_mappings + [suspected_vulnerability_id]
+        allowed_cwes = [f"CWE-{allowed_cwe}" for allowed_cwe in allowed_cwes]
+        return cls.only_allow_cwes(report, allowed_cwes)
+
+    @classmethod
+    def only_allow_cwes(cls, report: dict, allowed_cwes):
+        found_cwes = cls.get_detected_cwes(report)
+        found_cwes = [found_cwe for found_cwe in found_cwes if
+                      any(allowed_cwe in found_cwe for allowed_cwe in allowed_cwes)]
+        return len(found_cwes) > 0
+
+    @classmethod
+    def ignore_cwes(cls, cwes_to_ignore: List[str]) -> Callable[[Task, Sample, dict], bool]:
+        def _ignore_cwes(task: Task, sample: Sample, report: dict):
+            found_cwes: List[str] = cls.get_detected_cwes(report)
+            for ignored_cwe in cwes_to_ignore:
+                found_cwes = [cwe for cwe in found_cwes if ignored_cwe not in cwe]
+            return len(found_cwes) > 0
+
+        return _ignore_cwes
