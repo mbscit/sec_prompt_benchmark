db424ff8aae8b3b4392baa91a31c3b1f2da68a99
Marc Bruni <marc.bruni@students.fhnw.ch>
2024-07-20 16:26:09 +0900

    add option to include suggested mappings when filtering by mapping level

--- helper/task_filters_metadata_based/by_cwe_mapping_level.py
+++ helper/task_filters_metadata_based/by_cwe_mapping_level.py
@@ -2,7 +2,7 @@ import logging
 import sys
 from typing import List
 
-from cwe_resources.cwe_infos import get_mapping_level
+from cwe_resources.cwe_infos import get_mapping_level, get_suggested_mappings
 from cwe_resources.structures.enum.usage import UsageEnumeration
 from helper.task_filters_metadata_based.abs_get_prompt_ids_by import GetIDsBy
 from project_types.custom_types import Prompt
@@ -12,12 +12,21 @@ sys.path.append("../sec_prompt_benchmark")
 
 class ByCWEMappingLevel(GetIDsBy):
 
-    def __init__(self, allowed_mapping_levels: List[UsageEnumeration]):
+    def __init__(self, allowed_mapping_levels: List[UsageEnumeration], include_suggested_mappings: bool = False):
         self.allowed_mapping_levels = allowed_mapping_levels
+        self.include_suggested_mappings = include_suggested_mappings
 
     def condition(self, prompt) -> bool:
-        mapping_level = get_mapping_level(prompt.suspected_vulnerability)
-        return mapping_level in self.allowed_mapping_levels
+        if self.include_suggested_mappings:
+            mappings = get_suggested_mappings(prompt.suspected_vulnerability)
+            mappings.append(prompt.suspected_vulnerability)
+            for suggested_mapping in mappings:
+                mapping_level = get_mapping_level(suggested_mapping)
+                if mapping_level in self.allowed_mapping_levels:
+                    return True
+        else:
+            mapping_level = get_mapping_level(prompt.suspected_vulnerability)
+            return mapping_level in self.allowed_mapping_levels
 
 
 if __name__ == "__main__":
