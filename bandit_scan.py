5b58379d8875d8a7001762dcc29c9e0fdf9c4ab6
Marc Bruni <marc.bruni@students.fhnw.ch>
2024-07-20 21:14:26 +0900

    implement bandit error extraction

--- bandit_scan.py
+++ bandit_scan.py
@@ -42,53 +42,30 @@ class BanditScanner:
 
     def extract_scan_results(self, bandit_result, task: Task, sample_index: int):
         file_name = self.get_file_name(task)
-        file_path = os.path.join(str(sample_index), file_name)
+        file_path = os.path.join("python", str(sample_index), file_name)
 
-        file_specific_results = [result for result in bandit_result["runs"][0]["results"] if
-                                 any(location for location in result["locations"] if
-                                     file_path == location["physicalLocation"]["artifactLocation"]["uri"])]
-        for file_specific_result in file_specific_results:
-            rule = next(rule for rule in bandit_result["runs"][0]["tool"]["driver"]["rules"] if
-                        rule["id"] == file_specific_result["ruleId"])
-
-            file_specific_result["rule"] = rule
+        file_specific_results = [result for result in bandit_result["results"] if
+                                 result["filename"].endswith(file_path)]
 
         return file_specific_results
 
     def extract_scan_errors(self, bandit_result, task: Task, sample_index: int):
-        file_name = self.get_file_name(task)
-        file_path = os.path.join(str(sample_index), file_name)
+        if bandit_result["errors"]:
+            file_name = self.get_file_name(task)
+            file_path = os.path.join("python", str(sample_index), file_name)
 
-        try:
+            file_specific_errors = [result for result in bandit_result["errors"] if
+                                    result["filename"].endswith(file_path)]
 
-            execution_notifications = [result for result in
-                                       bandit_result["runs"][0]["invocations"][0]["toolExecutionNotifications"]]
-            execution_notifications = [notification for notification in execution_notifications if
-                                       "locations" in notification]
-            file_specific_notifications = [execution_notification for execution_notification in execution_notifications
-                                           if
-                                           any(location for location in execution_notification["locations"] if
-                                               file_path == location["physicalLocation"]["artifactLocation"]["uri"])]
-            file_specific_extraction_warnings = [notification for notification in file_specific_notifications if
-                                                 notification["descriptor"]["id"].endswith("extraction-warnings")]
-            file_specific_extraction_errors = [notification for notification in file_specific_notifications if
-                                               notification["descriptor"]["id"].endswith(
-                                                   "failed-extractor-invocations")]
+            for file_specific_error in file_specific_errors:
+                logging.info(
+                    f"Bandit error for Task {task.id}, sample {sample_index}: \n {file_specific_error}")
+                self.errors.append(
+                    SampleError(task_id=task.id, sample_index=sample_index, error=file_specific_error['reason']))
 
-            file_specific_extraction_problems = file_specific_extraction_errors + file_specific_extraction_warnings
+            return file_specific_errors
+        return []
 
-            file_specific_errors = [file_specific_extraction_problem["properties"]["formattedMessage"]["text"] for
-                                    file_specific_extraction_problem in file_specific_extraction_problems]
-        except KeyError as e:
-            raise e
-
-        for file_specific_error in file_specific_errors:
-            logging.info(
-                f"Bandit error for Task {task.id}, sample {sample_index}: \n {file_specific_error}")
-            self.errors.append(
-                SampleError(task_id=task.id, sample_index=sample_index, error=file_specific_error))
-
-        return file_specific_errors
 
     @staticmethod
     def get_file_name(task: Task):
@@ -97,6 +74,7 @@ class BanditScanner:
             raise ValueError(f"Unsupported language {task.language}")
         file_name = f"{BanditScanner.encode_name(task.id)}.{file_extension}"
         return file_name
+
 
     def scan_samples(self, approach: Approach):
         working_dir = relative_path_from_root('./tmp_code')
@@ -147,75 +125,62 @@ class BanditScanner:
                             if not task.samples[sample_index].bandit_successfully_scanned:
                                 self.write_extracted_code(task, sample_index, sample_folder)
 
-                    database_path = os.path.join(language_folder, "bandit-database")
                     results_path = os.path.join(language_folder, "bandit-results.json")
 
-                    logging.info(f"Starting bandit database create for {language}")
-            #         if language != "python":
-            #             raise ValueError(f"Unsupported language {language}, bandit only supports python")
-            #         else:
-            #             database_create_command = f"bandit database create --language={language} --source-root=\"{language_folder}\" {database_path}"
-            #
-            #         create_result = subprocess.run(database_create_command, shell=True, capture_output=True,
-            #                                        text=True)
-            #         logging.info(f"Bandit database create result for language {language}: {create_result.stdout}")
-            #
-            #         if create_result.returncode != 0:
-            #             raise Exception(f"Bandit database creation failed for language {language}. "
-            #                             f"{create_result.stdout} "
-            #                             f" {create_result.stderr} ")
-            #
-            #         logging.info(f"Starting bandit database analyze for {language}")
-            #
-            #         database_analyze_command = f"bandit database analyze {database_path} --format=sarifv2.1.0 --output={results_path}"
-            #         analyze_result = subprocess.run(database_analyze_command, shell=True, capture_output=True,
-            #                                         text=True)
-            #         logging.info(f"Bandit database analyze result for language {language}: {analyze_result.stdout}")
-            #
-            #         if analyze_result.returncode == 0:
-            #             json_output = json.loads(open(results_path, 'r').read())
-            #
-            #             for task in tasks_to_consider:
-            #
-            #                 for sample_index in range(num_samples):
-            #                     try:
-            #                         sample: Sample = next(
-            #                             (sample for sample in task.samples if sample.index == sample_index),
-            #                             None)
-            #                         if not sample.bandit_successfully_scanned:
-            #                             file_specific_errors = self.extract_scan_errors(json_output, task,
-            #                                                                             sample_index)
-            #                             sample.bandit_scanner_report = self.extract_scan_results(json_output, task,
-            #                                                                                      sample_index)
-            #
-            #                             if not file_specific_errors:
-            #                                 sample.bandit_successfully_scanned = True
-            #                                 self.successful_scans += 1
-            #                             else:
-            #                                 sample.bandit_successfully_scanned = False
-            #                                 self.error_samples += 1
-            #
-            #                     except Exception as e:
-            #                         logging.error(
-            #                             f"Error processing results for {task.id} sample {sample_index}: {e}")
-            #                         self.errors.append(
-            #                             SampleError(task_id=task.id, sample_index=sample_index, error=str(e)))
-            #                         self.error_samples += 1
-            #
-            #                     approach.update_errors("bandit_scan", self.errors, sample_index)
-            #
-            #         else:
-            #             raise Exception(
-            #                 f"Bandit database analyze failed for language {language}, sample {sample_index}. "
-            #                 f"{analyze_result.stdout} "
-            #                 f" {analyze_result.stderr} ")
-            #
-            # print(f"Summary:")
-            # print(f"Total Samples: {len(tasks) * num_samples}")
-            # print(f"Successful Bandit Scans: {self.successful_scans}")
-            # print(f"Skipped Samples: {self.skipped_samples}")
-            # print(f"Error Samples: {self.error_samples}")
-        # shutil.rmtree(subfolder)
+                    logging.info(f"Starting bandit scan")
+                    if language != "python":
+                        raise ValueError(f"Unsupported language {language}, bandit only supports python")
+
+                    logging.info(f"Starting bandit scan")
+
+                    bandit_scan_command = f"bandit -r {language_folder} -f json --output={results_path}"
+                    analyze_result = subprocess.run(bandit_scan_command, shell=True, capture_output=True,
+                                                    text=True)
+                    logging.info(f"Bandit scan command output: {analyze_result.stdout}")
+
+                    if analyze_result.returncode == 0 or analyze_result.returncode == 1:
+                        json_output = json.loads(open(results_path, 'r').read())
+
+                        for task in tasks_to_consider:
+
+                            for sample_index in range(num_samples):
+                                try:
+                                    sample: Sample = next(
+                                        (sample for sample in task.samples if sample.index == sample_index),
+                                        None)
+                                    file_specific_errors = self.extract_scan_errors(json_output, task,
+                                                                                    sample_index)
+                                    sample.bandit_scanner_report = self.extract_scan_results(json_output, task,
+                                                                                             sample_index)
+
+                                    if not file_specific_errors:
+                                        sample.bandit_successfully_scanned = True
+                                        self.successful_scans += 1
+                                    else:
+                                        sample.bandit_successfully_scanned = False
+                                        self.error_samples += 1
+
+                                except Exception as e:
+                                    logging.error(
+                                        f"Error processing results for {task.id} sample {sample_index}: {e}")
+                                    self.errors.append(
+                                        SampleError(task_id=task.id, sample_index=sample_index, error=str(e)))
+                                    self.error_samples += 1
+
+                                approach.update_errors("bandit_scan", self.errors, sample_index)
+
+                    else:
+                        raise Exception(
+                            f"Bandit scan failed for sample {sample_index}. "
+                            f"{analyze_result.stdout} "
+                            f" {analyze_result.stderr} ")
+
+            print(f"Summary:")
+            print(f"Total Samples: {len(tasks) * num_samples}")
+            print(f"Successful Bandit Scans: {self.successful_scans}")
+            print(f"Skipped Samples: {self.skipped_samples}")
+            print(f"Error Samples: {self.error_samples}")
+        shutil.rmtree(subfolder)
 
 
 if __name__ == "__main__":
