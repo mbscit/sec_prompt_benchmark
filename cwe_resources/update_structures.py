c830838bee34b344134cb6cd89d35fdaad28a531
Marc Bruni <marc.bruni@students.fhnw.ch>
2024-07-28 16:51:40 +0900

    add CanAlsoBe from CWE-1000 to suspected vulnerabilities on attempt creation

--- cwe_resources/update_structures.py
+++ cwe_resources/update_structures.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-from cwe_resources.update_scripts import create_detection_methods, create_enums
+from cwe_resources.update_scripts import create_detection_methods, create_enums, create_can_also_be
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__))))
 
@@ -14,6 +14,7 @@ def main():
     create_cwe_hierarchy.create_cwe_hierarchy_structure()
     create_mapping_usage.create_mapping_usage_structure()
     create_suggested_mappings.create_suggested_mappings_structure()
+    create_can_also_be.create_can_also_be_structure()
     create_detection_methods.create_detection_methods_structure()
 
     create_enums.create_enums()
