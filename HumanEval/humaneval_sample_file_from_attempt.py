6174167bb7d3ebb828500be5b17a7f0ff53791b0
Marc Bruni <marc.bruni@students.fhnw.ch>
2024-08-11 17:44:41 +0200

    convert HumanEval attempts to required format for HumanEval benchmark

---
+++ HumanEval/humaneval_sample_file_from_attempt.py
@@ -0,0 +1,28 @@
+import json
+import os
+
+from dotenv import load_dotenv
+
+import utils
+from project_types.custom_types import Approach
+
+load_dotenv()
+
+data_file_path = utils.relative_path_from_root(os.getenv("DATA_FILE_PATH"))
+approach: Approach = utils.read_approaches_file(data_file_path)
+
+samples = []
+
+for task in approach.tasks:
+    for sample in task.samples:
+        samples.append({
+            "task_id": task.id,
+            "completion": sample.extracted_code
+        })
+
+# write to jsonl file
+output_file_name = approach.id + "-" + approach.model + ".jsonl"
+output_file_path = utils.relative_path_from_root("HumanEval/samples/" + output_file_name)
+with open(output_file_path, "w") as file:
+    for sample in samples:
+        file.write(f"{json.dumps(sample)}\n")
