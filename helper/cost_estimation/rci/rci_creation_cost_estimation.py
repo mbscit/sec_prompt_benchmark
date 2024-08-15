f3acffeb6c019d7c439093bc9b851ca17a46f52f
Marc Bruni <marc.bruni@students.fhnw.ch>
2024-08-15 03:04:56 +0200

    generate csv of rci creation estimation

--- helper/cost_estimation/rci/rci_creation_cost_estimation.py
+++ helper/cost_estimation/rci/rci_creation_cost_estimation.py
@@ -1,6 +1,8 @@
+import csv
 import logging
 import os
 
+import pandas as pd
 import tiktoken
 from dotenv import load_dotenv
 
@@ -48,6 +50,14 @@ def analyze(base: Approach, result: Approach):
     total_4o_input_tokens, total_4o_output_tokens = get_no_tokens(base, result, encoding)
     total_4o_cost = get_cost_for_model(model, total_4o_input_tokens, total_4o_output_tokens)
 
+    model = "gpt-4o"
+    encoding = tiktoken.encoding_for_model(model)
+    total_4o_input_tokens, total_4o_output_tokens = get_no_tokens(base, result, encoding)
+    total_4o_cost = get_cost_for_model(model, total_4o_input_tokens, total_4o_output_tokens)
+
+    model = "gpt-4o-2024-08-06"
+    total_4o_2024_08_06_cost = get_cost_for_model(model, total_4o_input_tokens, total_4o_output_tokens)
+
     model = "gpt-4o-mini"
     encoding = tiktoken.encoding_for_model(model)
     total_4o_mini_input_tokens, total_4o_mini_output_tokens = get_no_tokens(base, result, encoding)
@@ -58,13 +68,26 @@ def analyze(base: Approach, result: Approach):
         "GPT-4 Cost": total_4_cost,
         "GPT-4-turbo Cost": total_4_turbo_cost,
         "GPT-4o Cost": total_4o_cost,
+        "GPT-4o-2024-08-06 Cost": total_4o_2024_08_06_cost,
         "GPT-4o-mini Cost": total_4o_mini_cost,
     }
 
     print(results)
 
+    return results
+
 
 if __name__ == "__main__":
+
+    matrix = []
+
+
+
+
+    sort_column = "Filename"
+    matrix.sort(key=lambda row: row[sort_column])
+
+
     load_dotenv()
     data_folder_path = os.path.dirname(utils.relative_path_from_root(os.getenv("DATA_FILE_PATH")))
 
@@ -72,12 +95,46 @@ if __name__ == "__main__":
 
     base = utils.read_approaches_file(os.path.join(data_folder_path, "baseline.json"))
     result = utils.read_approaches_file(os.path.join(data_folder_path, "ptfscg_rci-from-baseline-iteration-1.json"))
-    analyze(base, result)
+    results = {"Filename": os.path.basename("ptfscg_rci-from-baseline-iteration-1.json")}
+    results.update(analyze(base, result))
+    matrix.append(results)
+
+    base = utils.read_approaches_file(os.path.join(data_folder_path, "pe-01-a.json"))
+    result = utils.read_approaches_file(os.path.join(data_folder_path, "ptfscg_rci-from-baseline-iteration-1.json"))
+    results = {"Filename": os.path.basename("ptfscg_rci-from-pe-03-a.json")}
+    results.update(analyze(base, result))
+    matrix.append(results)
 
     base = utils.read_approaches_file(os.path.join(data_folder_path, "ptfscg_rci-from-baseline-iteration-1.json"))
     result = utils.read_approaches_file(os.path.join(data_folder_path, "ptfscg_rci-from-baseline-iteration-2.json"))
-    analyze(base, result)
+    results = {"Filename": os.path.basename("ptfscg_rci-from-baseline-iteration-2.json")}
+    results.update(analyze(base, result))
+    matrix.append(results)
 
     base = utils.read_approaches_file(os.path.join(data_folder_path, "ptfscg_rci-from-baseline-iteration-2.json"))
     result = utils.read_approaches_file(os.path.join(data_folder_path, "ptfscg_rci-from-baseline-iteration-3.json"))
     analyze(base, result)
+    results = {"Filename": os.path.basename("ptfscg_rci-from-baseline-iteration-3.json")}
+    results.update(analyze(base, result))
+    matrix.append(results)
+
+    print_matrix = pd.DataFrame.from_records(
+        matrix,
+        columns=[
+            "Filename",
+            "GPT-3.5-turbo Cost",
+            "GPT-4 Cost",
+            "GPT-4-turbo Cost",
+            "GPT-4o Cost",
+            "GPT-4o-2024-08-06 Cost",
+            "GPT-4o-mini Cost"
+        ],
+    ).to_string(index=False, header=True)
+
+    print(print_matrix)
+
+    with open("rci_creation_cost_estimation.csv", "w+") as output:
+        if matrix:
+            csvWriter = csv.DictWriter(output, matrix[0].keys(), quoting=csv.QUOTE_ALL)
+            csvWriter.writeheader()
+            csvWriter.writerows(matrix)
