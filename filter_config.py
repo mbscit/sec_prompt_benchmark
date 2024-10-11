9233a9aab53243c651a6f063503d493d91d4c50b
Marc Bruni <marc.bruni@students.fhnw.ch>
2024-10-11 23:04:08 +0200

    cleanup * remove obsolete filters from config examples * remove obsolete Makefile

--- filter_config.py
+++ filter_config.py
@@ -10,8 +10,6 @@ SEMGREP_SCAN_RESULT_FILTERS: List[Callable[[Task, Sample, dict], bool]] = [
     # SemgrepScanResultFilters.min_confidence(SemgrepConfidence.MEDIUM),
     # SemgrepScanResultFilters.min_severity(SemgrepSeverity.WARNING),
     # SemgrepScanResultFilters.cwe_relatives_of_suspected(allow_ancestors=False, allow_peers=False, allow_descendants=True),
-    # SemgrepScanResultFilters.cwe_in_recommended_mapping,
-    # SemgrepScanResultFilters.cwe_in_can_also_be,
     # SemgrepScanResultFilters.affected_code_in_generated_response,
     SemgrepScanResultFilters.only_suspected_cwe,
 ]
@@ -22,8 +20,6 @@ CODEQL_SCAN_RESULT_FILTERS: List[Callable[[Task, Sample, dict], bool]] = [
     # CodeqlScanResultFilters.min_problem_severity(CodeqlProblemSeverity.WARNING),
     # CodeqlScanResultFilters.min_security_severity(0),
     # CodeqlScanResultFilters.cwe_relatives_of_suspected(allow_ancestors=False, allow_peers=False, allow_descendants=True)
-    # CodeqlScanResultFilters.cwe_in_recommended_mapping,
-    # CodeqlScanResultFilters.cwe_in_can_also_be,
     # codeql_scan_result_filters.affected_code_in_generated_response,
     CodeqlScanResultFilters.only_suspected_cwe,
 ]
